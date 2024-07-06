96896c9947f2177d6d26708ac03b1e61696098dd
Shahin Kahrizi <kahrizi.1992@gmail.com>
2024-07-06 07:56:18 +0330

    add app loading

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+const loadingElement = document.querySelector('.app-loading') as HTMLDivElement;
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    setTimeout(() => loadingElement.remove(), 500);
+  })
   .catch((err) => console.error(err));
