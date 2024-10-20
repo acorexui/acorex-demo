27a6c0ee7ec14b8692f5c7977178d83bf50072a1
Shahin Kahrizi <kahrizi.1992@gmail.com>
2024-10-20 13:58:00 +0330

    fix media viewer

--- src/app/cases/media-viewer/usage/usage.component.ts
+++ src/app/cases/media-viewer/usage/usage.component.ts
@@ -23,29 +23,29 @@ export class UsageComponent {
   protected data = signal<AXMediaViewerData[]>([
     {
       id: '1',
-      name: 'test image',
+      name: 'Library',
       type: 'image',
       size: '128kb',
       url: 'https://picsum.photos/id/20/500/500',
-      thumbnail: 'https://picsum.photos/id/25/200/300',
+      thumbnail: 'https://picsum.photos/id/20/500/500',
     },
     {
       id: '2',
-      name: 'test audio',
+      name: 'Audio',
       type: 'audio',
       size: '256kb',
       url: 'https://actions.google.com/sounds/v1/ambiences/kids_playing.ogg',
     },
     {
       id: '3',
-      name: 'test pdf',
+      name: 'Test pdf',
       type: 'pdf',
       size: '512kb',
       url: 'https://www.buds.com.ua/images/Lorem_ipsum.pdf',
     },
     {
       id: '4',
-      name: 'test video',
+      name: 'Animation',
       type: 'video',
       size: '1024kb',
       url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
@@ -53,19 +53,19 @@ export class UsageComponent {
     },
     {
       id: '5',
-      name: 'test2 image',
+      name: 'Coffee',
       type: 'image',
       size: '290kb',
       url: 'https://picsum.photos/id/30/500/500',
-      thumbnail: 'https://picsum.photos/id/35/200/300',
+      thumbnail: 'https://picsum.photos/id/30/500/500',
     },
     {
       id: '6',
-      name: 'test3 image',
+      name: 'Rain',
       type: 'image',
       size: '300kb',
       url: 'https://picsum.photos/id/41/500/500',
-      thumbnail: 'https://picsum.photos/id/45/200/300',
+      thumbnail: 'https://picsum.photos/id/41/500/500',
     },
   ]);
 }
