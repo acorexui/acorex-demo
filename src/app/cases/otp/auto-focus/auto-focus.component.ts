973d014b051b406fcb2b651a991f0b00aaa83b05
Shahin Kahrizi <kahrizi.1992@gmail.com>
2024-10-28 11:47:01 +0330

    add auto focus tp otp

--- src/app/cases/otp/auto-focus/auto-focus.component.ts
+++ src/app/cases/otp/auto-focus/auto-focus.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AXOtpModule } from '@acorex/components/otp';
+import { AXCommonModule } from '@acorex/components/common';
 
 @Component({
   standalone: true,
   templateUrl: 'auto-focus.component.html',
-  imports: [AXOtpModule],
+  imports: [AXOtpModule, AXCommonModule],
 })
 export class AutoFocusComponent {}
