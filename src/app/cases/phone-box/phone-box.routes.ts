19725dfcbcf313770d4dabb2a41551329092062a
Mojtaba Erfan Rad <m.erfanrad@demisco.com>
2024-11-20 16:35:03 +0330

    added route

--- src/app/cases/phone-box/phone-box.routes.ts
+++ src/app/cases/phone-box/phone-box.routes.ts
@@ -3,18 +3,29 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', redirectTo: 'validation', pathMatch: 'full' },
   {
+    path: 'usage',
+    loadComponent: () =>
+      import('./usage/usage.component').then((e) => e.UsageComponent),
+  },
+  {
     path: 'validation',
     loadComponent: () =>
-      import('./validation/validation.component').then((e) => e.ValidationComponent),
+      import('./validation/validation.component').then(
+        (e) => e.ValidationComponent
+      ),
   },
   {
     path: 'only-countries',
     loadComponent: () =>
-      import('./only-countries/only-countries.component').then((e) => e.OnlyCountriesComponent),
+      import('./only-countries/only-countries.component').then(
+        (e) => e.OnlyCountriesComponent
+      ),
   },
   {
     path: 'exclude',
     loadComponent: () =>
-      import('./only-countries/only-countries.component').then((e) => e.OnlyCountriesComponent),
+      import('./only-countries/only-countries.component').then(
+        (e) => e.OnlyCountriesComponent
+      ),
   },
 ];
