106b445e570498f5935a6c32beeca24743854bf2
Mojtaba Erfan Rad <m.erfanrad@demisco.com>
2024-11-20 16:22:55 +0330

    fix imports

--- src/app/cases/alert/usage/usage.component.ts
+++ src/app/cases/alert/usage/usage.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 
-import { AXBadgeModule } from '@acorex/components/badge';
 import { AXAlertModule } from '@acorex/components/alert';
 import { AXDecoratorModule } from '@acorex/components/decorators';
-import { AXButtonModule } from '@acorex/components/button';
 
 @Component({
   standalone: true,
   templateUrl: 'usage.component.html',
-  imports: [AXBadgeModule, AXAlertModule, AXDecoratorModule, AXButtonModule],
+  imports: [AXAlertModule, AXDecoratorModule],
 })
 export class UsageComponent {}
