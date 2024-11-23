6d7f7f59af61c007ca374ee7ab51870a1788e7ce
Shahin Kahrizi <kahrizi.1992@gmail.com>
2024-11-23 11:13:39 +0330

    add mock data to html file

--- src/app/cases/json-viewer/usage/usage.component.ts
+++ src/app/cases/json-viewer/usage/usage.component.ts
@@ -1,7 +1,6 @@
 import { AXJsonViewerModule } from '@acorex/components/json-viewer';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import mockJson from '../mockData.json';
 
 @Component({
   templateUrl: './usage.component.html',
@@ -9,5 +8,24 @@ import mockJson from '../mockData.json';
   imports: [FormsModule, AXJsonViewerModule],
 })
 export class UsageComponent {
-  mockData = signal(mockJson);
+  mockData = signal({
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: { lat: -37.3159, lng: 81.1496 },
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets',
+    },
+  });
 }
