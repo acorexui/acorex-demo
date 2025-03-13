f39a00a42c4116d9118262ff7f9f5f125e1eab7d
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2025-03-13 12:04:20 +0330

    fixed custom header case

--- src/app/cases/collapse/custom-header-template/custom-header-template.component.ts
+++ src/app/cases/collapse/custom-header-template/custom-header-template.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
 import { AXCollapseModule } from '@acorex/components/collapse';
 import { AXImageModule } from '@acorex/components/image';
+import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: 'custom-header-template.component.html',
-    imports: [AXCollapseModule, AXImageModule]
+  templateUrl: 'custom-header-template.component.html',
+  imports: [AXCollapseModule, AXImageModule],
 })
 export class CustomHeaderTemplate {
   protected _options: { color: string; isAccordion: boolean; look: any } = {
+    look: 'solid',
     color: 'primary',
     isAccordion: false,
-    look: 'solid',
   };
 }
