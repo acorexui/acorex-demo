08a72c2cd4d26c72cd08bce3cfe99509cc8ea77a
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2025-03-17 08:38:30 +0330

    fixed build problem

--- src/app/cases/tag-box/usage/usage.component.ts
+++ src/app/cases/tag-box/usage/usage.component.ts
@@ -2,7 +2,6 @@ import { AXValueChangedEvent } from '@acorex/components/common';
 import { AXTagBoxComponent } from '@acorex/components/tag-box';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import console from 'console';
 
 @Component({
   templateUrl: 'usage.component.html',
