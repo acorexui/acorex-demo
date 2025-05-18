bdf071fdb7c5fb91db0493d08ad2d700a2a05441
Shahin Kahrizi <kahrizi.1992@gmail.com>
2025-05-18 15:10:36 +0330

    Enhance time duration component by integrating AXTimeDurationModule, updating usage template, and adding signal-based state management for start and end values. Update translations for date and time terms in English and Persian.

--- src/app/cases/time-duration/usage/usage.component.ts
+++ src/app/cases/time-duration/usage/usage.component.ts
@@ -1,9 +1,39 @@
 import { AXButtonModule } from '@acorex/components/button';
 import { AXTooltipModule } from '@acorex/components/tooltip';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  AXTimeDurationModule,
+  AXTimeDurationUnit,
+} from '@acorex/components/time-duration';
+import { AXValueChangedEvent } from '@acorex/cdk/common';
 
 @Component({
   templateUrl: 'usage.component.html',
-  imports: [AXButtonModule, AXTooltipModule],
+  imports: [AXButtonModule, AXTooltipModule, AXTimeDurationModule, FormsModule],
 })
-export class UsageComponent {}
+export class UsageComponent {
+  protected value = 0;
+  protected valueStart = signal<AXTimeDurationUnit>('YEAR');
+  protected valueEnd = signal<AXTimeDurationUnit>('MILLISECOND');
+
+  protected options = signal({
+    readonly: false,
+    disabled: false,
+    look: 'solid',
+  });
+
+  protected onValueChanged(event: AXValueChangedEvent<number>) {
+    console.log(event.value);
+  }
+
+  protected onFirstRangeChange(event: AXValueChangedEvent<AXTimeDurationUnit>) {
+    this.valueStart.set(event.value!);
+  }
+
+  protected onSecondRangeChange(
+    event: AXValueChangedEvent<AXTimeDurationUnit>
+  ) {
+    this.valueEnd.set(event.value!);
+  }
+}
