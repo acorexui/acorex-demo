ebcdfffe449863ed5b8497c07cb914d28be61dcf
mani hoshyari <manihoshiaripour83@gmail.com>
2026-09-07 09:02:50 +0330

    add shimmer cases

---
+++ src/app/cases/cdk/shimmer/custom-color/custom-color.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+import { AXShimmerDirective } from '@acorex/cdk/shimmer';
+@Component({
+  templateUrl: 'custom-color.component.html',
+  imports: [AXShimmerDirective],
+})
+export class customColorComponent {}
---
+++ src/app/cases/cdk/shimmer/usage/usage.component.ts
@@ -0,0 +1,8 @@
+import { AXShimmerDirective } from '@acorex/cdk/shimmer';
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'usage.component.html',
+  imports: [AXShimmerDirective],
+})
+export class usageComponent {}
